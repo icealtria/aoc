a96f28c9adb40b3ed944b04ce1f60825cd190020
icealtria <icealtria@gmail.com>
2023-02-02 13:04:42 +0800

    write day1 in rust

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,21 @@
-use std::env;
 use std::fs;
 
 fn main() {
     let content = fs::read_to_string("input").expect("Should have been able to read the file");
 
-    let v: Vec<&str> = content.split("\n\n").collect();
-    println!("{:?}", v);
+    let calories: Vec<&str> = content.trim().split("\n\n").collect();
+
+    // part1
+    let mut calories: Vec<i32> = calories
+        .iter()
+        .map(|s| s.split('\n').map(|line| line.parse::<i32>().unwrap()).sum())
+        .collect();
+    let part1 = calories.iter().max().unwrap();
+    println!("part1 result: {part1}");
+
+    // part2
+    let len = calories.len();
+    calories.sort();
+    let part2: i32 = calories[len - 3..].iter().sum();
+    println!("part2 result: {part2}");
 }
