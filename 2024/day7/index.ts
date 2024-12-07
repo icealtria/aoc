6112012a0f55f33a3ee702294b1d5c3681867d7c
icealtria <icealtria@gmail.com>
2024-12-07 22:45:59 +0800

    faster

--- 2024/day7/index.ts
+++ 2024/day7/index.ts
@@ -1,4 +1,4 @@
-import { example as input } from "./input.ts";
+import { input } from "./input.ts";
 
 const equations = input.split("\n").map((line) => {
   const [v, ...numbers] = line.split(":").map((part) => part.trim());
@@ -25,7 +25,8 @@ const can_equal2 = (v: number, numbers: number[]): boolean => {
   if (numbers.length === 1) return numbers[0] === v;
   const mul = numbers[0] * numbers[1];
   const add = numbers[0] + numbers[1];
-  const concat = +`${numbers[0]}${numbers[1]}`;
+  // const concat = +`${numbers[0]}${numbers[1]}`;
+  const concat = numbers[0] * Math.pow(10, Math.floor(Math.log10(numbers[1]) + 1)) + numbers[1];
   return can_equal2(v, [concat, ...numbers.slice(2)]) ||
     can_equal2(v, [mul, ...numbers.slice(2)]) ||
     can_equal2(v, [add, ...numbers.slice(2)]);
